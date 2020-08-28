14ec0cd02f23aec766bfc7e43c89ddb013d48f62
rohan163 <51785842+rohan163@users.noreply.github.com>
2020-08-28 12:09:49 +0530

    24by7 server edit

--- test.py
+++ test.py
@@ -1,36 +1,70 @@
+import os
 import sys
-import datetime
-import socket
+import warnings
+warnings.simplefilter("ignore")
+import csv
+import nltk
+from sklearn import metrics
+import pandas as pd
+import numpy as np 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from collections import Counter
+from subprocess import check_output
+from wordcloud import WordCloud, STOPWORDS
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn import metrics
+import itertools
+from sklearn.metrics import precision_score,recall_score
+from sklearn.metrics import confusion_matrix
+import pickle
 
-def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+with open('news_file.csv', mode='w') as news_file:
+            filename = 'finalized_model.sav'
+            data = "%s" % (sys.argv[1]) 
+            loaded_model = pickle.load(open(filename, 'rb'))
+            news_writer = csv.writer(news_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            news_writer.writerow([data])
+news_set = pd.read_csv("news_file.csv", sep=',')
+news_set_data = news_set
+stop_list = stopwords.words('english')
+stemmer = PorterStemmer()
+all_tokens_lower = [t.lower() for t in news_set_data]
+tokens_normalised1 = [stemmer.stem(t) for t in all_tokens_lower
+                                       if t not in stop_list]
+final_testX = np.asarray(tokens_normalised1)
+predictedfinal = loaded_model.predict(final_testX)
+results =  predictedfinal
+if results == 1:
+    data = "Fake"
+else:
+    data = "True"
 
-    message = "%s" % (sys.argv[1])  # take input
-
-    
-    client_socket.send(message.encode())  # send message
-    data = client_socket.recv(1024).decode()  # receive response
-
-    if (data == "True"):
+if (data == "True"):
         sent="According To The Fact Checker The News Is:"
         sent2=""
-    else :
+else :
         sent="According To The Fact Checker The News Is:"
         sent2=". We Wont Recommend You Sharing This News Further. But We Would Still Request You To Cross Check The News From An Authentic News Channel."
-    
-    print(sent + data + sent2)  # show in terminal
-
-        
-
-    #client_socket.close()  # close the connection
-    
-
-if __name__ == '__main__':
-    client_program()
-
-#output="%s" % (sys.argv[1])
-#print(output)
+print(sent + data + sent2) 
+os.remove("news_file.csv")
